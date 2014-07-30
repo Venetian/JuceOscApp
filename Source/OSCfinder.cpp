c1a207bd1c67f3d5d501dd3aa855483a4188d014
Venetian <andrewrobertson@Andrews-MacBook-Pro-2.local>
2014-07-30 12:31:10 +0100

    added OscFinder class
    this is from mlrvst and it uses oscpack to grab some o/c messages being
    sent in

---
+++ Source/OSCfinder.cpp
@@ -0,0 +1,230 @@
+
+
+/*
+ ==============================================================================
+ 
+ OSCfinder.cpp
+ Created: 13 Sep 2011 7:03:15pm
+ Author:  Hemmer
+ 
+ ==============================================================================
+ */
+
+//#include "PluginProcessor.h"
+//#include "OSCfinder.h"
+#include "OSCfinder.h"
+
+
+//, mlrVSTAudioProcessor * const owner)
+
+
+
+
+/*if you get an error:
+libc++abi.dylib: terminating with uncaught exception of type std::runtime_error: unable to bind udp socket
+it may be something else is bound to this port alread
+ //when compiling this, if you have a std:runtime exception, check there's not something running binding to osc in background
+ //http://new-supercollider-mailing-lists-forums-use-these.2681727.n2.nabble.com/unable-to-bind-udp-socket-resolve-on-OS-X-td7604288.html
+ //http://forum.openframeworks.cc/t/ofxosc-unable-to-connect-udp-socket-crashes-of-app-when-in-login-items-mac-osx-mavericks/14943/5
+
+*/
+
+// Constructor
+OSCfinder:: OSCfinder(const String &prefix):
+            Thread("OscListener Thread"),
+            incomingPort(OSC_RECEIVE_PORT),
+            s(IpEndpointName("localhost", incomingPort), this),
+            buffer(), p(buffer, 1536),
+            transmitSocket(IpEndpointName("localhost", OSC_SENDER_PORT)),
+            OSCPrefix(prefix)
+{
+    setPrefix(prefix);
+}
+
+
+void OSCfinder::ProcessMessage(const osc::ReceivedMessage& m, const IpEndpointName& /*remoteEndpoint*/)
+{
+    const String stripWildcard = OSCPrefix+ "strip/*";
+    
+    try
+    {
+        String msgPattern = m.AddressPattern();
+        const int numArgs = m.ArgumentCount();
+        
+        //this isnt great - it assumes one argument which is a float
+        //need to work out a decent routine if taking this code forward
+        float tmp = getFloatOSCArg(m);
+        std::cout << "tmp " << tmp << std::endl;
+        
+        if (msgPattern.equalsIgnoreCase(OSCPrefix))
+        {
+            // we need three arguments for button presses
+            const int numArgs = m.ArgumentCount();
+            
+            std::cout << "rrceived " << numArgs << std::endl;
+            /*
+             if (numArgs != 3) throw osc::Exception();
+            
+            osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+            
+            // unpack the monome button, row and state (button up or down)
+            osc::int32 row, col, state;
+            args >> row >> col >> state >> osc::EndMessage;
+            //buttonPressCallback(row, col, state == 1);
+             */
+        }
+        else if (msgPattern.matchesWildcard(stripWildcard, false))
+        {
+            // strip off the /mlrvst/strip/ part of the message
+            msgPattern = msgPattern.substring(stripWildcard.length() - 1);
+            
+            // and extract the SampleStrip rowID from the message
+            const String rowIDStr = msgPattern.upToFirstOccurrenceOf("/", false, false);
+            
+           // const int stripID = rowIDStr.getIntValue();
+           // handleStripMessage(stripID, m);
+        } else {
+            std::cout << "receive something " << msgPattern << std::endl;
+        }
+    }
+    catch (osc::Exception& e)
+    {
+        DBG("error while parsing message: " << m.AddressPattern() << ": " << e.what() << "\n");
+    }
+}
+/*
+void OSCfinder::handleStripMessage(const int &stripID, const osc::ReceivedMessage& m)
+{
+    const String msgPattern = m.AddressPattern();
+    const String OSCcommand = msgPattern.fromFirstOccurrenceOf("/" + String(stripID) + "/", false, false);
+    
+    DBG("strip " << stripID << " sends command: " << OSCcommand);
+    
+    if (OSCcommand == "vol")
+    {
+        try
+        {
+            const float newVol = getFloatOSCArg(m);
+            parent->setSampleStripParameter(SampleStrip::pStripVolume, &newVol, stripID);
+        }
+        catch (osc::Exception &) { DBG("Couldn't process volume message"); }
+    }
+    
+    
+    else if (OSCcommand == "speed")
+    {
+        try
+        {
+            const float newSpeed = getFloatOSCArg(m);
+            parent->setSampleStripParameter(SampleStrip::pPlaySpeed, &newSpeed, stripID);
+        }
+        catch (osc::Exception &) { DBG("Couldn't process speed message"); }
+    }
+    
+    else if (OSCcommand == "chan")
+    {
+        try
+        {
+            const int newChannel = getIntOSCArg(m);
+            parent->setSampleStripParameter(SampleStrip::pCurrentChannel, &newChannel, stripID);
+        }
+        catch (osc::Exception &) { DBG("Couldn't process channel message"); }
+        
+    }
+}
+*/
+
+float OSCfinder::getFloatOSCArg(const osc::ReceivedMessage& m)
+{
+    try
+    {
+        // we assume only one argument
+        const int numArgs = m.ArgumentCount();
+        if (numArgs != 1) throw osc::MissingArgumentException();
+        
+        osc::ReceivedMessage::const_iterator arg = m.ArgumentsBegin();
+        
+        const float floatArg = arg->AsFloat();
+        
+        return floatArg;
+        
+    }
+    catch (osc::WrongArgumentTypeException& )
+    {
+        // if the argument is an int pretending to be a float, then we
+        // need to consider that too so we cast to float
+        try
+        {
+            osc::ReceivedMessage::const_iterator arg = m.ArgumentsBegin();
+            const float floatArg = (float) arg->AsInt32();
+            
+            return floatArg;
+        }
+        catch (osc::Exception &e)
+        {
+            throw e;
+        }
+    }
+    catch (osc::Exception &e)
+    {
+        // pass exception on
+        throw e;
+    }
+    
+}
+
+int OSCfinder::getIntOSCArg(const osc::ReceivedMessage& m)
+{
+    try
+    {
+        // we assume only one argument
+        const int numArgs = m.ArgumentCount();
+        if (numArgs != 1) throw osc::MissingArgumentException();
+        
+        osc::ReceivedMessage::const_iterator arg = m.ArgumentsBegin();
+        
+        const int intArg = arg->AsInt32();
+        
+        return intArg;
+        
+    }
+    catch (osc::Exception &e)
+    {
+        // pass exception on
+        throw e;
+    }
+}
+
+/*
+ void OSCfinder::buttonPressCallback(const int &monomeCol, const int &monomeRow, const bool &state)
+ {
+ if (state) { DBG("button down " << monomeRow << " " << monomeCol); }
+ else       { DBG("button up " << monomeRow << " " << monomeCol); }
+ 
+ parent->processOSCKeyPress(monomeCol, monomeRow, state);
+ }
+ 
+ void OSCfinder::setLED(const int &row, const int &col, const int &val)
+ {
+ p.Clear();
+ const char * msg = ledStr.getCharPointer();
+ p << osc::BeginMessage(msg) << row << col << val << osc::EndMessage;
+ transmitSocket.Send(p.Data(), p.Size());
+ }
+ 
+ void OSCfinder::setRow(const int &row, const int &val)
+ {
+ p.Clear();
+ const char * msg = ledRowStr.getCharPointer();
+ p << osc::BeginMessage(msg) << row << 0 << val << osc::EndMessage;
+ transmitSocket.Send(p.Data(), p.Size());
+ }
+ 
+ void OSCfinder::clearGrid()
+ {
+ p.Clear();
+ const char * msg = ledClearStr.getCharPointer();
+ p << osc::BeginMessage(msg) << osc::EndMessage;
+ transmitSocket.Send(p.Data(), p.Size());
+ }
+ */
---
+++ Source/OSCfinder.h
@@ -0,0 +1,128 @@
+//
+//  OSCfinder.h
+//  MidiSyncTracker
+//
+//  Created by Andrew Robertson on 30/07/2014.
+//
+//
+
+#ifndef __MidiSyncTracker__OSCfinder__
+#define __MidiSyncTracker__OSCfinder__
+
+#include <iostream>
+
+#endif /* defined(__MidiSyncTracker__OSCfinder__) */
+
+
+//
+//  OSCfinder.cpp
+//  MidiSyncTracker
+//
+//  Created by Andrew Robertson on 30/07/2014.
+//
+//
+
+//#include "OSCfinder.h"
+
+#ifndef __OSCFINDER__
+#define __OSCFINDER__
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+// OSC send includes
+#include "osc/OscOutboundPacketStream.h"
+#include "ip/IpEndpointName.h"
+// OSC receive includes
+#include "osc/OscReceivedElements.h"
+#include "osc/OscPacketListener.h"
+// OSC shared includes
+#include "ip/UdpSocket.h"
+
+//#include "AudioSample.h"
+
+// forward declaration
+//class MainContentComponent;//mlrVSTAudioProcessor;
+
+#define OSC_RECEIVE_PORT 7500//change for alternative port
+#define OSC_SENDER_PORT 8080//NOT USING?
+
+class OSCfinder :  public osc::OscPacketListener,
+public Thread
+{
+    
+public:
+    
+    // Constructor
+    OSCfinder(const String &prefix);//, MainContentComponent* const owner);//, mlrVSTAudioProcessor * const owner);
+    
+    ~OSCfinder()
+    {
+        // stop the OSC Listener thread running
+        s.AsynchronousBreak();
+        
+        // allow the thread 2 seconds to stop cleanly - should be plenty of time.
+        stopThread(2000);
+    }
+    
+    // Start the oscpack OSC Listener Thread
+    // NOTE: s.Run() won't return unless we force it to with
+    // s.AsynchronousBreak() as is done in the destructor
+    void run()
+    {
+        s.Run();
+    }
+    /*
+     void buttonPressCallback(const int &monomeCol, const int &monomeRow, const bool &state);
+     void setLED(const int &row, const int &col, const int &val);
+     void setRow(const int &row, const int &val);
+     void clearGrid();
+     */
+    void setPrefix(const String &prefix)
+    {
+        OSCPrefix = "/" + prefix;// + "/";
+     //   ledStr = OSCPrefix + "led";
+       // ledRowStr = OSCPrefix + "led_row";
+       // ledClearStr = OSCPrefix + "clear";
+       // buttonPressMask = OSCPrefix + "press";
+        
+        DBG("prefix now: " << OSCPrefix);
+    }
+    
+private:
+    // incoming /////////////////////////////
+    // for communication with PluginProcessor
+
+  //  MainContentComponent* const parent;
+    int incomingPort;
+    UdpListeningReceiveSocket s;
+    
+    
+    // outgoing messages ////////////////////
+    char buffer[1536];                  // to store message data
+    osc::OutboundPacketStream p;
+    UdpTransmitSocket transmitSocket;
+    
+    // strings ////////////////////////////
+    String OSCPrefix;                       // main prefix (/mlrvst/ by default)
+    //   String ledStr, ledRowStr, ledClearStr;  // + "led", + "led_row", + "clear"
+    //   String buttonPressMask;                 // + "press"
+    
+    //   void handleStripMessage(const int &stripID, const osc::ReceivedMessage& m);
+    
+    float getFloatOSCArg(const osc::ReceivedMessage& m);
+    int getIntOSCArg(const osc::ReceivedMessage& m);
+    
+    JUCE_LEAK_DETECTOR(OSCfinder);
+    
+protected:
+    
+    void ProcessMessage(const osc::ReceivedMessage& m, const IpEndpointName& /*remoteEndpoint*/);
+};
+
+
+#endif
+
+
+
+
+
